bf35b89b1663ecccd0bd7e572d48b5d9d8ce1925
Mateus Danielle <mateusdanielle@edu.fecap.br>
2020-10-20 13:17:19 -0300

    Removing unnecessary files ✂️

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import { Container } from './styles';
 
-export default App;
+const src: React.FC = () => {
+  return <h1>Hello World</h1>;
+};
+
+export default src;
